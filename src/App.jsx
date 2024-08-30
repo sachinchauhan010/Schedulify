fbe65533b878cbb6bdd036269d9b7bd5ada85101
sachinchauhan010 <010sssachin@gmail.com>
2024-08-30 09:40:28 +0530

    Project setup Done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from "@/components/theme-provider"
+function App() {
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    </ThemeProvider>
+  )
+}
+
+export default App
