aec0f25c96d10a9d46119bb49e5c57e04153f966
sachinchauhan010 <010sssachin@gmail.com>
2024-08-30 09:44:06 +0530

    fix: themeProvider import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { ThemeProvider } from "@/components/theme-provider"
 function App() {
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    </ThemeProvider>
+    <>Hello Schedulify</>
   )
 }
 
