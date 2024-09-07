9d3e90299f4bea58fbaf73be4a251266d0b313e0
alokverma <alokverma749@gmail.com>
2024-09-07 21:04:57 +0530

    fix: login ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,24 @@
-import Register from './Register.jsx'
-import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+import { Button } from "./ui/button"
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useState } from "react"
-import { Link } from "react-router-dom"
+} from "./ui/dialog"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
 
 export default function Login() {
   const [username, setUserName] = useState("")
   const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [isRegistering, setIsRegistering] = useState(false)
 
   return (
     <Dialog className="">
@@ -25,16 +27,15 @@ export default function Login() {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px] dark:text-[#fafafa]">
         <DialogHeader>
-          <DialogTitle>Login</DialogTitle>
+          <DialogTitle>{isRegistering ? 'Register' : 'Login'}</DialogTitle>
           {/* <DialogDescription>
             Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription> */}
         </DialogHeader>
         <div className="grid gap-4 py-4">
-
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
-              Username
+              User Name
             </Label>
             <Input id="username" value={username} className="col-span-3 rounded" onChange={(e) => setUserName(e.target.value)} />
           </div>
@@ -44,12 +45,24 @@ export default function Login() {
             </Label>
             <Input id="name" value={password} className="col-span-3 rounded" type="password" onChange={(e) => setPassword(e.target.value)} />
           </div>
+          {isRegistering &&
+            <>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="email" className="text-right">
+                  Email
+                </Label>
+                <Input id="email" value={email} className="col-span-3 rounded" onChange={(e) => setEmail(e.target.value)} />
+              </div>
+            </>
+          }
         </div>
         <DialogFooter className="dark:text-[#fafafa]">
-          <Button type="submit" className="border-[1px] border-white dark:text-[#fafafa] rounded">Cancel</Button>
-          <Button type="submit" className="dark:bg-[#fafafa] dark:text-[#09090b] rounded">Login</Button>
+          <DialogClose asChild>
+            <Button type="submit" className="border-[1px] border-white dark:text-[#fafafa] rounded">Cancel</Button>
+          </DialogClose>
+          <Button type="submit" className="dark:bg-[#fafafa] dark:text-[#09090b] rounded">{isRegistering ? 'Register' : 'Login'}</Button>
         </DialogFooter>
-        <Register />
+        <Button onClick={() => setIsRegistering(!isRegistering)}>{isRegistering ? 'Login' : 'Register'}</Button>
       </DialogContent>
     </Dialog>
   )
